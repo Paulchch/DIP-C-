1570a82a8956eeb78c64a0429c4ab794436f8b04
Paulchch <83615379+Paulchch@users.noreply.github.com>
2022-06-18 18:17:53 -0600

    Update

---
+++ src/Product.cpp
@@ -0,0 +1,34 @@
+//
+// Created by pabli on 18/6/2022.
+//
+
+#include "Product.h"
+
+Product::Product() {
+
+}
+
+Product::~Product() {
+
+}
+
+const std::string &Product::getName() const {
+    return name;
+}
+
+void Product::setName(const std::string &name) {
+    Product::name = name;
+}
+
+double Product::getPrice() const {
+    return price;
+}
+
+void Product::setPrice(double price) {
+    Product::price = price;
+}
+
+std::ostream &operator<<(std::ostream &os, const Product &product) {
+    os << "name: " << product.name << " price: " << product.price;
+    return os;
+}
---
+++ src/Product.h
@@ -0,0 +1,31 @@
+//
+// Created by pabli on 18/6/2022.
+//
+
+#ifndef LAB01_INHERITANCE_PRODUCT_H
+#define LAB01_INHERITANCE_PRODUCT_H
+#include<iostream>
+
+class Product {
+private:
+    std::string name;
+    double price;
+public:
+    Product();
+
+    virtual ~Product();
+
+    const std::string &getName() const;
+
+    void setName(const std::string &name);
+
+    double getPrice() const;
+
+    void setPrice(double price);
+
+    friend std::ostream &operator<<(std::ostream &os, const Product &product);
+
+};
+
+
+#endif //LAB01_INHERITANCE_PRODUCT_H
